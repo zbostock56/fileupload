b4e9e1746111310ff2932dc880c835d10180a1c9
Zack <zbostock56@gmail.com>
2023-07-04 17:47:34 -0400

    Functioning File Upload

--- server.js
+++ server.js
@@ -1,46 +1,43 @@
-const express = require("express")
-const path = require("path")
-const multer = require("multer")
-const app = express()
+const express = require("express");
+const multer = require('multer');
+const cors = require('cors');
 const port = 3000;
+const path = require("path")
+
+const app = express();
+app.use(cors()); // Allows incoming requests from any IP
 app.set("views",path.join(__dirname,"views"))
 app.set("view engine","ejs")
-	
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		// 'uploads' is the Upload_folder_name
-		cb(null, "uploads")
-	},
-	filename: function (req, file, cb) {
-	    cb(null, file.fieldname)
-	}
-})
-	
-const upload = multer({ storage: storage }).single("folder");	
 
-app.get("/", function(req,res){
-	res.render("signup");
+// Start by creating some disk storage options:
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + '/uploads');
+    },
+    // Sets file(s) to be saved in uploads folder in same directory
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+    // Sets saved filename(s) to be original filename(s)
+  })
+  
+// Set saved storage options:
+const upload = multer({ storage: storage })
+
+app.post("/api", upload.array("files"), (req, res) => {
+// Sets multer to intercept files named "files" on uploaded form data
+    console.log("Files uploaded successfully")
+    console.log(req.body); // Logs form body values
+    console.log(req.files); // Logs any files
+    res.json({ message: "File(s) uploaded successfully" });
+
+});
+
+app.get("/", function(req, res){
+	res.render("index");
+    console.log("*** Sent home ***")
 })
-	
-app.post("/upload", function (req, res, next) {		
-	upload (req,res,function(err) {
-		if(err instanceof multer.MulterError) {
-			res.send(err)
-            console.log("*** Multer Error ***")
-            console.log(err)
-		} else if (err) { 
-            res.send(err)
-            console.log("*** Unknown Error ***")
-            console.log(err)
-        } else {
-            console.log("Uploaded File: " + res.filename)
-			res.send("Success, Image uploaded!")
-            
-		}
-	})
-})
-	
-app.listen(port, function(error) {
-	if(error) throw error
-		console.log("Server created Successfully on PORT " + port)
-})
+
+app.listen(port, function() {
+    console.log("Server running on PORT " + port);
+});
