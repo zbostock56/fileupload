48a67315542b58757758bc487a12603d3190d35a
Zack <zbostock56@gmail.com>
2023-07-04 21:48:22 -0400

    Styling Changes

--- server.js
+++ server.js
@@ -18,19 +18,15 @@ const storage = multer.diskStorage({
     filename: function (req, file, callback) {
         callback(null, file.originalname);
     }
-    // Sets saved filename(s) to be original filename(s)
   })
   
-// Set saved storage options:
 const upload = multer({ storage: storage })
 
 app.post("/api", upload.array("files"), (req, res) => {
-// Sets multer to intercept files named "files" on uploaded form data
     console.log("Files uploaded successfully")
     console.log(req.body); // Logs form body values
     console.log(req.files); // Logs any files
     res.json({ message: "File(s) uploaded successfully" });
-
 });
 
 app.get("/", function(req, res){
